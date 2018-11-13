d74ad774055df3fdb16f77a4a099e1f2a0cc01a9
Ilya Dubovitsky <ilya.dubovitsky@gmail.com>
2018-11-13 19:34:48 +0800

    Cross domain request  handling added

--- be/settings.py
+++ be/settings.py
@@ -63,7 +63,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'be.urls'
 
